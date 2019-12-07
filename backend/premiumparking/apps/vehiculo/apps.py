655e5e35d07b6f456623c3f023126cc3280b0350
premiumparking5 <emanuelpatinogallo@jesusamigo.com.co>
2019-12-07 06:01:45 -0500

    apps

--- backend/premiumparking/apps/vehiculo/apps.py
+++ backend/premiumparking/apps/vehiculo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VehiculoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vehiculo'
+    name = 'apps.vehiculo'
--- backend/premiumparking/premiumparking/settings.py
+++ backend/premiumparking/premiumparking/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.facturacion',
+    'apps.horaparqueo',
+    'apps.permisos',
+    'apps.rol',
+    'apps.servicios',
+    'apps.usuarios',
+    'apps.vehiculos',
 ]
 
 MIDDLEWARE = [
