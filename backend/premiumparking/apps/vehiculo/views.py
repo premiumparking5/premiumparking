b085a325a77d7be74bef97fd83a729f12dce8217
premiumparking5 <emanuelpatinogallo@jesusamigo.com.co>
2025-06-19 16:57:54 -0500

    regristro

--- backend/premiumparking/apps/vehiculo/views.py
+++ backend/premiumparking/apps/vehiculo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
 
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .serializers import *
+
+
+class vehiculosViewSet(viewsets.ModelViewSet):
+
+    queryset = vehiculos.objects.all()
+    serializers_class = vehiculosSerializer
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = ([JWTAuthentication])
 # Create your views here.
