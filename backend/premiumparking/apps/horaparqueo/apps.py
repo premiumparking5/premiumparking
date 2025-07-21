b6dcd7a1eeadd0f87621d79d3cde1049de2cbf2c
premiumparking5 <emanuelpatinogallo@jesusamigo.com.co>
2025-07-21 15:59:33 -0500

    .

--- backend/premiumparking/apps/horaparqueo/apps.py
+++ backend/premiumparking/apps/horaparqueo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HoraparqueoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'horaparqueo'
+    name = 'apps.horaparqueo'
--- backend/premiumparking/apps/permisos/apps.py
+++ backend/premiumparking/apps/permisos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PermisosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'permisos'
+    name = 'apps.permisos'
